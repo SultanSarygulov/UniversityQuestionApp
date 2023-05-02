1f9982e2f37232b6f907d0762df3382253b60b49
SultanSarygulov <itsme.kaan.sarygulov@gmail.com>
2023-05-02 22:37:55 +0600

    Made AskQuestionFragment design more understandable

---
+++ app/src/main/java/com/example/uqa/presentation/askQuestion/AskQuestionFragment.kt
@@ -0,0 +1,68 @@
+package com.example.uqa.presentation.askQuestion
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.uqa.R
+import com.example.uqa.data.Post
+import com.example.uqa.databinding.FragmentAskQuestionBinding
+import com.example.uqa.databinding.FragmentQuestionBinding
+import com.example.uqa.presentation.home.HomeViewModel
+
+
+class AskQuestionFragment : Fragment() {
+
+    private lateinit var binding: FragmentAskQuestionBinding
+    private lateinit var viewModel: AskQuestionViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAskQuestionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[AskQuestionViewModel::class.java]
+
+        binding.postButton.setOnClickListener {
+            checkInputs()
+        }
+    }
+
+    private fun checkInputs() {
+        if (
+            binding.inputPostTextET.text.toString().isEmpty() ||
+            binding.inputName.text.toString().isEmpty()){
+            Toast.makeText(requireContext(), "Fill out all boxes!", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        postQuestion()
+    }
+
+    private fun postQuestion() {
+        val newPost = Post(
+            0,
+            binding.inputPostTextET.text.toString(),
+            binding.inputName.text.toString(),
+            "02/05/2023",
+            0,
+            0
+        )
+
+        viewModel.addPost(newPost)
+
+        Toast.makeText(requireContext(), "Question successfully posted!", Toast.LENGTH_SHORT).show()
+        findNavController().navigateUp()
+
+    }
+}
