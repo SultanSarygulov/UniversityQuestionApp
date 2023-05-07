fae761b99776716cd67ae88140aac70d039c1217
SultanSarygulov <itsme.kaan.sarygulov@gmail.com>
2023-05-07 18:32:57 +0600

    Added onboarding

---
+++ app/src/main/java/com/example/uqa/presentation/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.example.uqa.presentation
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.uqa.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, WelcomeActivity::class.java))
+        finish()
+    }
+}
